cdd47955035e20df0a52005f90c4c749934b1259
Roman Voronin <r.voronin@mobidev.biz>
2021-12-11 00:44:36 +0200

    123

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { HeaderComponent } from './header/header.component';
+import { MainBannerComponent } from './main-page/main-banner/main-banner.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    HeaderComponent,
+    MainBannerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
